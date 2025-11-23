4ae7023c052afe8e1ba465630c21e4ed86835107
Laaaain <laaaain@0x00.network>
2025-11-22 20:59:16 -0600

    fix: use rpm-ostree instead of dnf for coolercontrol and liquidctl
    installing coolercontrol through dnf does not create the systemd
    service causing `systemctl enable coolercontrold` to fail with
    "Failed to enable unit: Unit coolercontrold.service does not exist"
    
    installing with rpm-ostree should fix this as it did with netbird

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,13 +21,13 @@ repo_gpgcheck=1
 EOF
 
 # add coolercontrol copr
-wget https://copr.fedorainfracloud.org/coprs/codifryed/CoolerControl/repo/fedora-$(rpm -E %fedora)/codifryed-CoolerControl-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_codifryed-CoolerControl.repo
+# wget https://copr.fedorainfracloud.org/coprs/codifryed/CoolerControl/repo/fedora-$(rpm -E %fedora)/codifryed-CoolerControl-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_codifryed-CoolerControl.repo
 
 # install dnf plugins
-dnf5 install -y dnf-plugins-core
+# dnf5 install -y dnf-plugins-core
 
 # enable required copr repos
-dnf copr enable codifryed/CoolerControl
+# dnf copr enable codifryed/CoolerControl
 
 # remove bazaar
 dnf5 remove -y krunner-bazaar bazaar
@@ -37,13 +37,18 @@ dnf5 install -y plasma-discover plasma-discover-flatpak plasma-discover-notifier
   --exclude=plasma-discover-offline-updates,plasma-discover-packagekit,plasma-discover-rpm-ostree,packagekit
 
 # install other packages
-dnf5 install -y coolercontrol liquidctl podman-compose
+dnf5 install -y podman-compose
 
 # rpm-ostree install netbird to avoid post-install.sh error when using dnf
-rpm-ostree install netbird netbird-ui
+# must also install coolercontrol this way to get daemon to work or else
+# systemd service wont work.
+rpm-ostree install netbird netbird-ui coolercontrol liquidctl
 
 # enable podman socket
 systemctl enable podman.socket
 
 # enable netbird system service
 systemctl enable netbird
+
+# enable coolercontrold
+systemctl enable coolercontrold
