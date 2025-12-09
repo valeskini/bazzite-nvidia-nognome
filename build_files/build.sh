775025f7fd7384e575f829bdee25be9f7f6758bb
Massi Valeskini <massi@valeskini.dev>
2025-12-09 15:44:26 +0100

    Refactor build script to simplify package management
    Removed repository setup and package installation for Netbird and Terra, and cleaned up the package installation process.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,71 +2,17 @@
 
 set -ouex pipefail
 
-### Enable/Add repos
-
-# Add Netbird repo
-tee /etc/yum.repos.d/netbird.repo <<EOF
-[netbird]
-name=netbird
-baseurl=https://pkgs.netbird.io/yum/
-enabled=1
-gpgcheck=0
-gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
-repo_gpgcheck=1
-EOF
-
-# Enable Terra repository, credit to ublue for this code
-terra_repo="/etc/yum.repos.d/terra.repo"
-if (! grep -q "enabled=0" "$terra_repo"); then
-  echo "Terra repository already enabled."
-else
-  echo "Enabling Terra Repository."
-  sed -i 's@enabled=0@enabled=1@g' "$terra_repo"
-fi
-
-### Remove unwanted packages
-
 dnf5 remove -y krunner-bazaar \
   bazaar \
-  ptyxis
+  ptyxis \
+  gnome-disk-utility
 
-### Install packages
-
-# install discover, exclude packages that cause issues
 dnf5 install -y plasma-discover \
   plasma-discover-flatpak \
   plasma-discover-notifier \
   plasma-discover-kns \
+  kde-partitionmanager \
   --exclude=plasma-discover-offline-updates,plasma-discover-packagekit,plasma-discover-rpm-ostree,packagekit
 
-# install other packages
-dnf5 install -y podman-compose \
-  zsh \
-  util-linux \
-  vlc \
-  vlc-plugin-gstreamer \
-  vlc-plugin-ffmpeg \
-  vlc-plugin-pipewire \
-  vlc-plugins-all \
-  konsole \
-  neovim \
-  htop
-
-# install from bazzite-multilib copr
-dnf5 -y install --repo="copr:copr.fedorainfracloud.org:bazzite-org:bazzite-multilib" \
-  pipewire-config-raop
-
-# using rpm-ostree over dnf here as dnf had issues properly installing
-# these packages.
-rpm-ostree install -y netbird netbird-ui coolercontrol liquidctl
-
-### Enable services
-
-# enable podman socket
-systemctl enable podman.socket
-
-# enable netbird system service
-systemctl enable netbird
-
-# enable coolercontrold
-systemctl enable coolercontrold
+dnf5 install -y konsole \
+  kde-partitionmanager
